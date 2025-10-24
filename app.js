de879593e3b0eed0d32a917f97e1b3f74d970139
hrefabi <fabianasouzaivo@icloud.com>
2025-10-23 21:03:42 -0300

    create a time control automation

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const hour = new Date().getHours();
+const Shortcut = importModule("shortcuts")
+
+const ligarAtalho = "boa noite". 
+const desligarAtalho = "bom dia"
+
+
+if(hour >= 22 || hour < 6)
+  {
+    console.log("Executando automacao no modo noite...")
+    await Shortcut.run(desligarAtalho)
+  } 
+
+
 
